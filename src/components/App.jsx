10dd874a8023bdcd685a727c96ab1e3c288040cd
Igor Boiko <igorboiko@gmail.com>
2023-05-08 01:10:59 +0300

    add index pathes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,58 +1,40 @@
-import { NavLink, Route, Routes } from 'react-router-dom';
-import styled from 'styled-components';
+import { Route, Routes } from 'react-router-dom';
 import Home from '../pages/Home';
 import About from '../pages/About';
+import { Mission } from './Mission';
+import { Team } from './Team';
+import { Reviews } from './Reviews';
 import Products from '../pages/Products';
 import NotFound from '../pages/NotFound';
 import ProductsDetails from '../pages/ProductsDetails';
-import { Container, Header, Logo, Link } from './App.styled';
+import { SharedLayout } from './SharedLayout';
+// import { Container, Header, Logo, Link } from './App.styled';
 
-const StyledLink = styled(NavLink)`
-  color: black;
-  margin-right: 1rem;
+// const StyledLink = styled(NavLink)`
+//   color: black;
+//   margin-right: 1rem;
 
-  &.active {
-    color: orangered;
-  }
-`;
+//   &.active {
+//     color: orangered;
+//   }
+// `;
 
 export const App = () => {
   return (
-    <div
-      style={{
-        // height: '100vh',
-        // display: 'flex',
-        // justifyContent: 'center',
-        // alignItems: 'center',
-        fontSize: 20,
-        color: '#010101',
-      }}
-    >
-      <Container>
-        <Header>
-          <Logo>
-            <span role="img" aria-label="computer icon">
-              💻
-            </span>{' '}
-            GoMerch Store
-          </Logo>
-          <nav>
-            <Link to="/" end>
-              Home
-            </Link>
-            <Link to="/about">About</Link>
-            <Link to="/products">Products</Link>
-          </nav>
-        </Header>
-
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
+    <div>
+      <Routes>
+        <Route path="/" element={<SharedLayout />}>
+          <Route index element={<Home />} />
+          <Route path="/about" element={<About />}>
+            <Route path="mission" element={<Mission />} />
+            <Route path="team" element={<Team />} />
+            <Route path="reviews" element={<Reviews />} />
+          </Route>
           <Route path="/products" element={<Products />} />
           <Route path="/products/:id" element={<ProductsDetails />} />
           <Route path="*" element={<NotFound />} />
-        </Routes>
-      </Container>
+        </Route>
+      </Routes>
     </div>
   );
 };
